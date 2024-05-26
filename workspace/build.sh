47057880c33f4b3ad0b977fac31fd85eea58e362
ZWG <1941981477@qq.com>
2024-05-26 16:31:27 +0800

    add YOLOv10 build

--- workspace/build.sh
+++ workspace/build.sh
@@ -4,4 +4,8 @@ TRTEXEC=/home/jarvis/lean/TensorRT-8.6.1.6/bin/trtexec
 
 # export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/jarvis/lean/TensorRT-8.6.1.6/lib
 
+# rt-detr
 ${TRTEXEC} --onnx=rtdetr-l.onnx --minShapes=images:1x3x640x640 --optShapes=images:1x3x640x640 --maxShapes=images:16x3x640x640 --saveEngine=rtdetr-l.FP32.trtmodel
+
+# yolov10
+# ${TRTEXEC} --onnx=yolov10s.onnx --minShapes=images:1x3x640x640 --optShapes=images:1x3x640x640 --maxShapes=images:16x3x640x640 --saveEngine=yolov10s.FP32.trtmodel
